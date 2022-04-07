9c53fde256cb2432c2516efc615bae982d9a3292
shekhar <92559527+shekharmenon1@users.noreply.github.com>
2022-04-07 17:11:09 +1000

    Update and rename GUI.java to Maze.java

---
+++ MajorProject/src/Maze.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+public class Maze extends JFrame implements ActionListener, Runnable{
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
