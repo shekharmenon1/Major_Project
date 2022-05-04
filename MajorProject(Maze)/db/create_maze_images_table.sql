05b355f2e88778b6ea3497d2dcf6320b631821ef
shekhar <92559527+shekharmenon1@users.noreply.github.com>
2022-05-05 01:38:35 +1000

    deleting uneeded

--- MajorProject(Maze)/db/create_maze_images_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE Maze_Images (
-    Maze_ID INT,
-    Image_ID INT,
-    X_coord INT,
-   Y_coord INT,
-   Size INT
-);
--- MajorProject(Maze)/db/create_maze_master_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE Maze_Mazter (
-    Maze_ID INT,
-    Maze_Name INT,
-    Author_Name INT,
-   Create_Date datetime,
-   Last_Modified_Date datetime
-);
